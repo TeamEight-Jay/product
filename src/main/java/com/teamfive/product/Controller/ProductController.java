9306675229711658a9407b135546fb476c0cc0e6
sachinsekhar <sach.sekh.131997@gmail.com>
2019-01-19 15:21:45 +0530

    Added all services and controller

--- src/main/java/com/teamfive/product/Controller/ProductController.java
+++ src/main/java/com/teamfive/product/Controller/ProductController.java
@@ -1,8 +1,43 @@
 package com.teamfive.product.Controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.teamfive.product.DTO.ProductDTO;
+import com.teamfive.product.Entity.ProductEntity;
+import com.teamfive.product.Services.ProductServices;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    ProductServices productServices;
+
+    @PostMapping("/product/add")
+    public ProductDTO add(@RequestBody ProductDTO productDTO)
+    {
+        ProductEntity productEntity=new ProductEntity();
+        productServices.add(productDTO);
+        BeanUtils.copyProperties(productDTO,productEntity);
+        return productDTO;
+    }
+
+    @GetMapping("/product/get")
+    public ProductDTO get(@RequestParam String id){
+        ProductEntity productEntity= productServices.get(id);
+        ProductDTO productDTO=new ProductDTO();
+        BeanUtils.copyProperties(productEntity,productDTO);
+        return productDTO;
+    }
+
+    @GetMapping("product/getMini")
+    public ProductDTO getMini(@RequestParam String id){
+        ProductEntity productEntity=productServices.getMiniProduct(id);
+        ProductDTO productDTO=new ProductDTO();
+        BeanUtils.copyProperties(productEntity,productDTO);
+        return productDTO;
+    }
+
+
 }
