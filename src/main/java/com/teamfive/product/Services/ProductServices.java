a7d4132b6a62c1ff4ef23beb24b33b6529a6a375
sachinsekhar <sach.sekh.131997@gmail.com>
2019-01-19 15:28:35 +0530

    Added all services and controller

--- src/main/java/com/teamfive/product/Services/ProductServices.java
+++ src/main/java/com/teamfive/product/Services/ProductServices.java
@@ -4,6 +4,7 @@ package com.teamfive.product.Services;
 import com.teamfive.product.DTO.ProductDTO;
 import com.teamfive.product.Entity.ProductEntity;
 import com.teamfive.product.Repositories.ProductRepositories;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,16 +20,24 @@ public class ProductServices implements ProductInterface {
 
     @Override
     public ProductEntity add(ProductDTO productDTO) {
-        return null;
+        ProductEntity productEntity=new ProductEntity();
+        BeanUtils.copyProperties(productDTO,productEntity);
+        return productRepositories.save(productEntity);
     }
 
     @Override
     public ProductEntity get(String id) {
-        return null;
+        return productRepositories.findOne(id);
     }
 
     @Override
     public ProductEntity getMiniProduct(String id) {
-        return null;
+        ProductEntity productEntity=new ProductEntity();
+        productEntity=productRepositories.findOne(id);
+        ProductEntity miniProductEntity=new ProductEntity();
+        miniProductEntity.setImageSrc(productEntity.getImageSrc());
+        miniProductEntity.setProductId(productEntity.getProductId());
+        miniProductEntity.setProductName(productEntity.getProductName());
+        return  miniProductEntity;
     }
 }
