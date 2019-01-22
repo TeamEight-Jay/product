fe943b8d10085deeaf1102eeb06439d183f56e84
sachinsekhar <sach.sekh.131997@gmail.com>
2019-01-22 09:32:04 +0530

    Added cataegories search to interface

--- src/main/java/com/teamfive/product/Services/ProductInterface.java
+++ src/main/java/com/teamfive/product/Services/ProductInterface.java
@@ -4,11 +4,14 @@ import com.teamfive.product.DTO.ProductDTO;
 import com.teamfive.product.Entity.ProductEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface ProductInterface {
 
     ProductEntity add(ProductDTO productDTO);
     ProductEntity get(String id);
     ProductEntity getMiniProduct(String id);
+    List<ProductEntity> findByCataegory
 
 }
